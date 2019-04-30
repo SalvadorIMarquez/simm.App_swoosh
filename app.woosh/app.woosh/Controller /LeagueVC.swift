e2cc6ddb9d726d7f1fdde438ae7f4bc8c4a77225
Salvador Marquez <imt.simm@hotmail.com>
2019-04-30 16:27:02 -0500

    Aplicacion de prueba terminada. Solo se usa para ejemplificar el uso de botones y navegacion entre VC

--- app.woosh/app.woosh/Controller /LeagueVC.swift
+++ app.woosh/app.woosh/Controller /LeagueVC.swift
@@ -10,13 +10,42 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player : Player!
+    
+    @IBOutlet weak var btnNext: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillCVSegue", sender: self)
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectdLeague(leagueType: "men")
+        
+    }
+
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectdLeague(leagueType: "women")
+    }
+    
+
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectdLeague(leagueType: "coed")
+    }
+    
+    func selectdLeague(leagueType : String){
+        player.desiredLeague = leagueType
+        btnNext.isEnabled = true
+    }
+    
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let  skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
 }
---
+++ app.woosh/app.woosh/Model/player.swift
@@ -0,0 +1,15 @@
+//
+//  player.swift
+//  app.woosh
+//
+//  Created by Salvador Marquez on 4/30/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel : String!
+    
+}
